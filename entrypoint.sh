b49962fb865fa20d3381bfbee3c60636dd95a138
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-09-09 16:26:55 -0300

    Remove exit on failure for unbound-anchor in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p /var/lib/unbound
 # Baixa a trust anchor se não existir
 if [ ! -f /var/lib/unbound/root.key ]; then
     echo "Downloading DNSSEC trust anchor..."
-    unbound-anchor -a /var/lib/unbound/root.key || exit 1
+    unbound-anchor -a /var/lib/unbound/root.key
 fi
 
 # Inicia o Unbound em modo foreground
